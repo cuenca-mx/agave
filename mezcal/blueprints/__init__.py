4b42881a956129246ded35f5ecb5995e00f72b78
Glen <lccbrokha@gmail.com>
2020-08-21 11:12:37 -0500

    remove-authed

--- mezcal/blueprints/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['AuthedBlueprint', 'AuthedRestApiBlueprint', 'RestApiBlueprint']
-
-from .authed import AuthedBlueprint
-from .rest_api import RestApiBlueprint
-
-
-class AuthedRestApiBlueprint(AuthedBlueprint, RestApiBlueprint):
-    ...
