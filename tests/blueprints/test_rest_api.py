29bb09e7264162c0ad5477a127b337225cc16cf7
Glen <lccbrokha@gmail.com>
2020-08-27 16:29:25 -0500

    changed-tests

--- tests/blueprints/test_rest_api.py
+++ tests/blueprints/test_rest_api.py
@@ -1,70 +1,34 @@
-import datetime as dt
-import json
-
 import pytest
-from chalice import Chalice
+from chalice import Chalice, Response
 from chalice.test import Client
-from cuenca_validations.types import JSONEncoder
 
-from tests.conftest import app as bp
+from tests.conftest import app as app_demo
 
-today = dt.date.today()
-now = dt.datetime.now()
-utcnow = now.astimezone(dt.timezone.utc)
+bp = Chalice('myapp')
+bp.register_blueprint(app_demo)
 
 
-@pytest.fixture
+@app_demo.resource('/mytest')
+class DummyRestApi:
+    @staticmethod
+    def create() -> Response:
+        user_id = 'USH827492'
+        return Response(user_id, status_code=200)
+
+    @staticmethod
+    def delete(id: str) -> Response:
+        id_dummy = id
+        return Response(id_dummy, status_code=200)
+
+
+@pytest.fixture(scope='class')
 def app():
     app = Chalice('myapp')
-    app.register_blueprint(bp)
-
-    @app.route('/')
-    def health_check():
-        return dict(greeting="I'm healthy!!!")
-
-    @bp.post('/test')
-    def test_post():
-        return {'success': True}
-
-    @bp.patch('/patch')
-    def test_patch():
-        return {'only': 'patch'}
-
-    @bp.delete('/remove')
-    def test_remove():
-        return {'remove': 'all'}
-
-    @bp.validate(JSONEncoder)
-    def test_validate():
-        to_encode = dict(
-            status='succeeded', time=utcnow.isoformat(), hello='there'
-        )
-        encoded = json.dumps(to_encode, cls=JSONEncoder)
-        decoded = json.loads(encoded)
-
-        return decoded
-
+    app.register_blueprint(app_demo)
     return app
 
 
-def test_get_rest_api(app):
+def test_create_dummy(app) -> None:
     with Client(app) as client:
-        response = client.http.get('/')
-        assert response.json_body == {'greeting': "I'm healthy!!!"}
-
-
-def test_rest_api_blueprint(app) -> None:
-    with Client(app) as client:
-        response = client.http.get('/healthy_auth')
-
+        response = client.http.post('/mytest')
         assert response.status_code == 200
-
-
-def test_rest_api_validations(app):
-    @bp.resource('/proof')
-    def test_patch():
-        return {'only': 'resource'}
-
-    with Client(app) as client:
-        response = client.http.post('/proof')
-        assert response.status_code == 403
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
-from agave.blueprints.rest_api import RestApiBlueprint
+from agave.blueprints import RestApiBlueprint
 
 app = RestApiBlueprint(__name__)
 
 
 @app.get('/healthy_auth')
 def health_auth_check() -> Dict:
-    return dict(greeting="only healthy !!!")
+    return dict(greeting="I'm authenticated and healthy !!!")
