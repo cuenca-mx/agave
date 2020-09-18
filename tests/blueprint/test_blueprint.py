0b4e9ac87fc26ffc86b3eb318ec8528fdc89152a
Glen <lccbrokha@gmail.com>
2020-09-18 11:41:25 -0500

    adding-test

--- tests/blueprint/test_blueprint.py
+++ tests/blueprint/test_blueprint.py
@@ -20,6 +20,15 @@ def test_create_transfer(app, user_creds: Dict, transfer_dict: Dict) -> None:
         assert response.status_code == 200
 
 
+def test_create_transfer_bad_request(app, user_creds: Dict) -> None:
+    with Client(app) as client:
+        transfer = dict(foobar='foobar')
+        response = client.http.post(
+            '/mytest', headers=user_creds['auth'], body=json.dumps(transfer)
+        )
+        assert response.status_code == 400
+
+
 @patch('agave.blueprints.rest_api.AUTHORIZER', 'AWS_IAM')
 def test_query_all_transfers(app, user_creds: Dict) -> None:
     with Client(app) as client:
@@ -32,9 +41,32 @@ def test_query_all_transfers(app, user_creds: Dict) -> None:
 
 
 @patch('agave.blueprints.rest_api.AUTHORIZER', 'AWS_IAM')
+def test_invalid_params(app, user_creds: Dict) -> None:
+    with Client(app) as client:
+        wrong_params = dict(wrong_param='wrong_value')
+        response = client.http.get(
+            f'/mytest?{urlencode(wrong_params)}', headers=user_creds['auth']
+        )
+        assert response.status_code == 400
+
+
+@patch('agave.blueprints.rest_api.AUTHORIZER', 'AWS_IAM')
 def test_query_all_transfers_with_limit(app, user_creds: Dict) -> None:
     with Client(app) as client:
         query_params = dict(limit=1)
+        response = client.http.get(
+            f'/mytest?{urlencode(query_params)}', headers=user_creds['auth']
+        )
+        assert response.status_code == 200
+        assert len(response.json_body['items']) == 1
+
+
+@patch('agave.blueprints.rest_api.AUTHORIZER', 'AWS_IAM')
+def test_query_all_transfers_with_page_size_and_limit(
+    app, user_creds: Dict
+) -> None:
+    with Client(app) as client:
+        query_params = dict(page_size=2, limit=5)
         response = client.http.get(
             f'/mytest?{urlencode(query_params)}', headers=user_creds['auth']
         )
@@ -81,3 +113,9 @@ def test_update_card_bad_request(client: Client, virtual_card: Card) -> None:
     virtual_card.reload()
     assert resp.status_code == 400
     assert virtual_card.status == CardStatus.created
+
+
+def test_update_not_existing_card(client: Client) -> None:
+    card_update_req = CardUpdateRequest(status=CardStatus.blocked)
+    resp = client.http.patch('/cards/NOT_EXISTS', json=card_update_req.dict())
+    resp.status_code == 404
