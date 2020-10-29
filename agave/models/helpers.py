0985c4556b2b8663c59cf9db96622c32944dcf52
Glen <lccbrokha@gmail.com>
2020-10-29 12:31:38 -0600

    update-code

--- agave/models/helpers.py
+++ agave/models/helpers.py
@@ -3,9 +3,10 @@ import datetime as dt
 import uuid
 from base64 import urlsafe_b64encode
 from enum import Enum
-from typing import Type
+from typing import Dict, List, NamedTuple, Type
 
 from blinker.base import NamedSignal
+from bson import SON, DBRef
 from mongoengine import (
     BooleanField,
     ComplexDateTimeField,
@@ -16,7 +17,9 @@ from mongoengine import (
     EmbeddedDocument,
     EmbeddedDocumentField,
     FloatField,
+    GenericLazyReferenceField,
     IntField,
+    LazyReferenceField,
     ListField,
     signals,
 )
@@ -67,6 +70,55 @@ class EnumField(BaseField):
         )
 
 
+class MapEntrie(NamedTuple):
+    ids: List[str]
+    models: List[str]
+
+
+class DynamicLazyReferenceField(GenericLazyReferenceField):
+    mapper: Dict
+    models: Dict[str, Document]
+
+    mapper_ids = (
+        MapEntrie(ids=['TR', 'SP', 'LT'], models=['Deposit', 'Transfer']),
+        MapEntrie(ids=['CD'], models=['Deposit']),
+        MapEntrie(ids=['ST'], models=['BillPayment']),
+        MapEntrie(ids=['CT'], models=['CardTransaction']),
+        MapEntrie(ids=['SW'], models=['WhatsappTransfer']),
+        MapEntrie(ids=['CO', 'PA'], models=['Commission']),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.models = {model._class_name: model for model in kwargs['choices']}
+        self.mapper = kwargs['mapper'] if 'mapper' in kwargs else {}
+
+    def __set__(self, instance, value):
+        if not value or '_ref' in value:
+            return super().__set__(instance, value)
+
+        model_names = next(
+            (data.models for data in self.mapper_ids if value[:2] in data.ids),
+            [],
+        )
+
+        model = next(
+            (
+                self.models[model_name]
+                for model_name, enums in self.mapper.items()
+                if instance._data['type'] in enums and len(model_names) > 1
+            ),
+            self.models[model_names[0]] if model_names else None,
+        )
+        document = None
+        if isinstance(value, str) and model:
+            collection = model._get_collection_name()
+            ref = DBRef(collection, value)
+            document = SON((("_cls", model._class_name), ("_ref", ref)))
+
+        return super().__set__(instance, document)
+
+
 def mongo_to_dict(obj, exclude_fields: list = None) -> dict:
     """
     from: https://gist.github.com/jason-w/4969476
@@ -88,7 +140,6 @@ def mongo_to_dict(obj, exclude_fields: list = None) -> dict:
             continue
 
         data = obj._data[field_name]
-
         if isinstance(obj._fields[field_name], ListField):
             return_data[field_name] = list_field_to_dict(data)
         elif isinstance(obj._fields[field_name], EmbeddedDocumentField):
@@ -97,6 +148,16 @@ def mongo_to_dict(obj, exclude_fields: list = None) -> dict:
             return_data[field_name] = data
         elif isinstance(obj._fields[field_name], EnumField):
             return_data[field_name] = data.value if data else None
+        elif isinstance(obj._fields[field_name], LazyReferenceField):
+            return_data[f'{field_name}_uri'] = (
+                f'/{data._DBRef__collection}/{data.id}' if data else None
+            )
+        elif isinstance(obj._fields[field_name], GenericLazyReferenceField):
+            return_data[f'{field_name}_uri'] = (
+                f'/{data["_ref"]._DBRef__collection}/{data["_ref"].id}'
+                if data
+                else None
+            )
         else:
             return_data[field_name] = mongo_to_python_type(
                 obj._fields[field_name], data
@@ -111,6 +172,8 @@ def list_field_to_dict(list_field: list) -> list:
     for item in list_field:
         if isinstance(item, EmbeddedDocument):
             return_data.append(mongo_to_dict(item))
+        elif isinstance(item, Enum):
+            return_data.append(item.value)
         else:
             return_data.append(mongo_to_python_type(item, item))
 
