ba95116befe8b68f5298a0d1098f4688b3be95d7
Glen <lccbrokha@gmail.com>
2020-10-29 11:55:36 -0600

    fix-conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,11 @@
-import datetime as dt
-from typing import Dict, Generator, List
+from typing import Dict, Generator
 
 import pytest
 from chalice.test import Client
 
 from agave.models.helpers import uuid_field
-from tests.chalicelib.model_transfer import TransactionStatus, Transfer
 
-from .helpers import auth_header, collection_fixture
+from .helpers import auth_header
 
 
 @pytest.fixture()
@@ -19,17 +17,6 @@ def client() -> Generator[Client, None, None]:
 
 
 @pytest.fixture
-def transfer_dict() -> Dict:
-    return dict(
-        account_number='646180157034181180',
-        amount=10000,
-        descriptor='mezcal, pulque y tequila',
-        recipient_name='Doroteo Arango',
-        idempotency_key='some unique key',
-    )
-
-
-@pytest.fixture
 def user_creds() -> Generator[Dict, None, None]:
     sk = 'cuenca2020'
     user_id = uuid_field('US')()
@@ -38,33 +25,3 @@ def user_creds() -> Generator[Dict, None, None]:
         user_id=user_id,
         auth=auth_header(id, sk),
     )
-
-
-@pytest.fixture
-@collection_fixture(Transfer)
-def transfers(user_creds: Dict) -> List[Transfer]:
-    return [
-        Transfer(
-            created_at=dt.datetime.utcnow() - dt.timedelta(days=1),
-            account_number='123456789123456789',
-            recipient_name='Frida Kahlo',
-            amount=10000,
-            descriptor='Transfer Test',
-            idempotency_key='key_1',
-            status=TransactionStatus.submitted,
-        ),
-        Transfer(
-            created_at=dt.datetime.utcnow() - dt.timedelta(hours=2),
-            account_number='123456789123456789',
-            recipient_name='Frida Kahlo',
-            amount=200000,
-            descriptor='Transfer Test',
-            idempotency_key='key_2',
-            status=TransactionStatus.succeeded,
-        ),
-    ]
-
-
-@pytest.fixture
-def transfer(transfers: List[Transfer]) -> Generator[Transfer, None, None]:
-    yield transfers[0]
