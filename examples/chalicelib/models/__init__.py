4f7e3d1fa9535353fcb9f61ef1288236bcf9ddd8
Glen <lccbrokha@gmail.com>
2020-11-30 18:19:58 -0600

    add-code

--- examples/chalicelib/models/__init__.py
+++ examples/chalicelib/models/__init__.py
@@ -1,6 +1,5 @@
-__all__ = ['Account', 'Transaction', 'AccountRedis', 'TransactionRedis']
+__all__ = ['Account', 'Transaction', 'TransactionRedis']
 
 from .accounts import Account
-from .models_redis.accounts import AccountRedis
 from .models_redis.transactions import TransactionRedis
 from .transactions import Transaction
