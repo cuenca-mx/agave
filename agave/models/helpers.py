a97ee766f7ed367f45fb185405d76d26eea34ed7
keryc Diaz <kerycdiaz@gmail.com>
2021-03-11 11:56:18 -0600

    new uuid_field_account

--- agave/models/helpers.py
+++ agave/models/helpers.py
@@ -7,3 +7,9 @@ def uuid_field(prefix: str = ''):
         return prefix + urlsafe_b64encode(uuid.uuid4().bytes).decode()[:-2]
 
     return base64_uuid_func
+
+
+def uuid_field_account(account_number: str, bank_code: str, user_id: str):
+    text = f"{bank_code}{account_number}{user_id}"
+    uuid5 = uuid.uuid5(uuid.NAMESPACE_DNS, text)
+    return 'AC' + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
---
+++ tests/models/test_uuid_field_account.py
@@ -0,0 +1,11 @@
+from agave.models.helpers import uuid_field_account
+
+def test_uuid_field_account():
+    data = dict(
+        account_number='123456789',
+        bank_code='90464',
+        user_id='US01'
+    )
+    account_id = uuid_field_account(**data)
+    assert 'AC' in account_id
+    assert account_id == 'ACg4Jt4p6FUTKhIg12j2csQw'
