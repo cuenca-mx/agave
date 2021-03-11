c05acd30e69b33e33398e6f95dcb4bbfdb7f4d58
keryc Diaz <kerycdiaz@gmail.com>
2021-03-11 12:22:56 -0600

    make format 2

--- agave/models/helpers.py
+++ agave/models/helpers.py
@@ -9,7 +9,9 @@ def uuid_field(prefix: str = ''):
     return base64_uuid_func
 
 
-def uuid_field_account(account_number: str, bank_code: str, user_id: str) -> str:
+def uuid_field_account(
+    account_number: str, bank_code: str, user_id: str
+) -> str:
     text = f"{bank_code}{account_number}{user_id}"
     uuid5 = uuid.uuid5(uuid.NAMESPACE_DNS, text)
     return 'AC' + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
