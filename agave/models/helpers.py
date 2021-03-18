64cdd068034e31e3996f5ad95320923eb8afb714
keryc Diaz <kerycdiaz@gmail.com>
2021-03-18 16:07:27 -0600

    fix uuid generic

--- agave/models/helpers.py
+++ agave/models/helpers.py
@@ -9,9 +9,12 @@ def uuid_field(prefix: str = ''):
     return base64_uuid_func
 
 
-def uuid_field_account(
-    account_number: str, bank_code: str, user_id: str
-) -> str:
-    text = f"{bank_code}{account_number}{user_id}"
-    uuid5 = uuid.uuid5(uuid.NAMESPACE_DNS, text)
-    return 'AC' + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
+def uuid_field_generic(prefix: str = '', **kwargs) -> str:
+    sorted_text = ''.join(
+        [
+            item[1]
+            for item in sorted(kwargs.items(), key=lambda x: x[0].lower())
+        ]
+    )
+    uuid5 = uuid.uuid5(uuid.NAMESPACE_OID, sorted_text)
+    return prefix + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
--- tests/models/test_uuid_field_account.py
+++ tests/models/test_uuid_field_account.py
@@ -1,8 +1,15 @@
-from agave.models.helpers import uuid_field_account
+from agave.models.helpers import uuid_field_generic
 
 
-def test_uuid_field_account():
-    data = dict(account_number='123456789', bank_code='90464', user_id='US01')
-    account_id = uuid_field_account(**data)
-    assert 'AC' in account_id
-    assert account_id == 'ACg4Jt4p6FUTKhIg12j2csQw'
+def test_uuid_field_generic():
+    values_asc = dict(
+        account_number='123456789', bank_code='90464', user_id='US01'
+    )
+    generic_asc = uuid_field_generic('AC', **values_asc)
+
+    values_desc = dict(
+        user_id='US01', bank_code='90464', account_number='123456789'
+    )
+    generic_desc = uuid_field_generic('AC', **values_desc)
+
+    assert generic_asc == generic_desc
