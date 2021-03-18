0004cf42268e9889a0601294b851c492f8379896
keryc Diaz <kerycdiaz@gmail.com>
2021-03-18 16:14:33 -0600

    fix tests

--- tests/models/test_uuid_field_account.py
+++ tests/models/test_uuid_field_account.py
@@ -5,17 +5,15 @@ from agave.models.helpers import uuid_field_generic
 
 
 def test_uuid_field_generic():
-    values_asc = dict(
+    generic_asc = uuid_field_generic('AC', **dict(
         account_number='123456789', bank_code='90464', user_id='US01'
-    )
-    generic_asc = uuid_field_generic('AC', **values_asc)
+    ))
 
     uuid5 = uuid.uuid5(uuid.NAMESPACE_OID, '12345678990464US01')
     assert generic_asc == 'AC' + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
 
-    values_desc = dict(
+    generic_desc = uuid_field_generic('AC', **dict(
         user_id='US01', bank_code='90464', account_number='123456789'
-    )
-    generic_desc = uuid_field_generic('AC', **values_desc)
+    ))
 
     assert generic_asc == generic_desc
