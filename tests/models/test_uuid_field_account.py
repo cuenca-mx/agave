dfc0058881cf2cffeda81a372c2740ff6e20cd62
keryc Diaz <kerycdiaz@gmail.com>
2021-03-18 16:14:52 -0600

    make format

--- tests/models/test_uuid_field_account.py
+++ tests/models/test_uuid_field_account.py
@@ -5,15 +5,17 @@ from agave.models.helpers import uuid_field_generic
 
 
 def test_uuid_field_generic():
-    generic_asc = uuid_field_generic('AC', **dict(
-        account_number='123456789', bank_code='90464', user_id='US01'
-    ))
+    generic_asc = uuid_field_generic(
+        'AC',
+        **dict(account_number='123456789', bank_code='90464', user_id='US01'),
+    )
 
     uuid5 = uuid.uuid5(uuid.NAMESPACE_OID, '12345678990464US01')
     assert generic_asc == 'AC' + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
 
-    generic_desc = uuid_field_generic('AC', **dict(
-        user_id='US01', bank_code='90464', account_number='123456789'
-    ))
+    generic_desc = uuid_field_generic(
+        'AC',
+        **dict(user_id='US01', bank_code='90464', account_number='123456789'),
+    )
 
     assert generic_asc == generic_desc
