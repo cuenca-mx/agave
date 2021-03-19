2e1ddb87c089b4bad1e62fef6400ce70d32bfb40
keryc Diaz <kerycdiaz@gmail.com>
2021-03-19 10:28:19 -0600

    fix comments of reviews

--- agave/models/helpers.py
+++ agave/models/helpers.py
@@ -9,11 +9,15 @@ def uuid_field(prefix: str = ''):
     return base64_uuid_func
 
 
-def uuid_field_generic(prefix: str = '', **kwargs) -> str:
+# This function is used to generate an id composed of a list of fields
+# in alphabetical order, for example if we want uuid_field_generic
+# ('AC', account_number='bla', user_id='ble') it will always
+# generate the same id, because it is based on The arguments
+def uuid_field_generic(prefix: str = '', **fields) -> str:
     sorted_text = ''.join(
         [
             item[1]
-            for item in sorted(kwargs.items(), key=lambda x: x[0].lower())
+            for item in sorted(fields.items(), key=lambda x: x[0].lower())
         ]
     )
     uuid5 = uuid.uuid5(uuid.NAMESPACE_OID, sorted_text)
