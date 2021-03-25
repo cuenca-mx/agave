a2359b15f09931fda2da6e11260c0e695a6b5caa
Ricardo <rsc@cuenca.com>
2021-03-24 18:01:26 -0600

    New cards (#69)
    * Cuenca-validations
    
    * More specific
    
    * Versions
    
    * Other version
    
    * Version
    
    * Using url_reference
    
    * Version
    
    * Using to_dict instead
    
    * Lint
    
    * Bump version
    
    * DEbug
    
    * Debug
    
    * Remove debug
    
    * Lint
    
    * Restore space
    
    * Restore files
    
    * Not needed test
    
    * Version
    
    * cuenca_validations

--- tests/lib/test_model_helpers.py
+++ tests/lib/test_model_helpers.py
@@ -1,6 +1,10 @@
+import re
 from datetime import datetime as dt
+from decimal import Decimal
 from enum import Enum
+from typing import ClassVar
 
+import pytest
 from mongoengine import (
     BooleanField,
     ComplexDateTimeField,
@@ -20,21 +24,24 @@ from mongoengine import (
 
 from agave.lib.mongoengine.enum_field import EnumField
 from agave.lib.mongoengine.model_helpers import mongo_to_dict
+from agave.models.base import BaseModel
 
 
-class Reference(Document):
-    pass
+class Reference(Document, BaseModel):
+    meta: ClassVar = {
+        'collection': 'references',
+    }
 
 
 class EnumType(Enum):
     member = 'name'
 
 
-class Embedded(EmbeddedDocument):
+class Embedded(EmbeddedDocument, BaseModel):
     name = StringField()
 
 
-class TestModel(Document):
+class TestModel(Document, BaseModel):
     str_field = StringField()
     int_field = IntField(default=1)
     float_field = FloatField(default=1.1)
@@ -55,14 +62,22 @@ class TestModel(Document):
     __test__ = False
 
 
-def test_mongo_to_dict():
-    assert not mongo_to_dict(None)
+@pytest.fixture
+def model():
     reference = Reference()
     reference.save()
     model = TestModel(
-        embedded_list_field=[Embedded(name='')], lazzy_list_field=[reference]
+        embedded_list_field=[Embedded(name='')],
+        lazzy_field=reference,
+        lazzy_list_field=[reference],
     )
     model.save()
+    model.reload()
+    return model
+
+
+def test_mongo_to_dict(model):
+    assert not mongo_to_dict(None)
     model_dict = mongo_to_dict(model, exclude_fields=['str_field'])
 
     assert 'id' in model_dict
@@ -70,7 +85,7 @@ def test_mongo_to_dict():
     assert 'complex_date_time_field' in model_dict
     assert model_dict['int_field'] == 1
     assert model_dict['float_field'] == '1.1'
-    assert model_dict['decimal_field'] == 1.2
+    assert model_dict['decimal_field'] == Decimal('1.2')
     assert model_dict['dict_field']['one'] == 1
     assert model_dict['enum_field'] == 'name'
     assert model_dict['boolean_field'] is True
@@ -78,6 +93,11 @@ def test_mongo_to_dict():
     assert model_dict['enum_list_field'] == ['name']
     assert model_dict['embedded_list_field'] == [{'name': ''}]
     assert model_dict['embedded_field'] == {}
-    assert model_dict['lazzy_field_uri'] is None
+    reference_reg = re.compile(r'\/references\/.{24}')
+    assert reference_reg.match(model_dict['lazzy_field_uri'])
+    assert len(model_dict['lazzy_list_field_uris']) == 1
+    assert all(
+        reference_reg.match(field)
+        for field in model_dict['lazzy_list_field_uris']
+    )
     assert model_dict['generic_lazzy_field_uri'] is None
-    assert model_dict['lazzy_list_field_uris'] == ["Reference object"]
