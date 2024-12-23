92211980b463818959e785e92b19e6090be5b5a8
gabino <gabino@cuenca.com>
2024-12-23 12:41:21 -0600

    Updated __get_value method to convert enum values to string format

--- agave/lib/mongoengine/enum_field.py
+++ agave/lib/mongoengine/enum_field.py
@@ -19,7 +19,7 @@ class EnumField(BaseField):
         super(EnumField, self).__init__(*args, **kwargs)
 
     def __get_value(self, enum: Enum) -> str:
-        return enum.value if hasattr(enum, 'value') else enum
+        return str(enum.value if hasattr(enum, 'value') else enum)
 
     def to_python(self, value: Enum) -> Enum:  # pragma: no cover
         return self.enum(super(EnumField, self).to_python(value))
