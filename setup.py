3659ed6afe996d347ed2a943cbc9ce5825519f49
gabino <gabino@cuenca.com>
2024-12-26 17:47:47 -0600

    Enhance setup.py to enforce installation with extras

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import sys
 from importlib.machinery import SourceFileLoader
 
 from setuptools import find_packages, setup
 
 version = SourceFileLoader('version', 'agave/version.py').load_module()
 
+
+if not any(arg.startswith('agave[') for arg in sys.argv):
+    sys.stderr.write(
+        "Error: You must install agave with either"
+        " [chalice_support] or [fast_support].\nExample:\n"
+        "  pip install 'agave[chalice_support]'\n"
+        "  pip install 'agave[fast_support]'\n"
+    )
+    sys.exit(1)
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -22,11 +32,12 @@ setup(
     include_package_data=True,
     package_data=dict(agave=['py.typed']),
     python_requires='>=3.8',
-    install_requires=[],
+    install_requires=[
+        'cuenca-validations===2.0.0.dev',
+    ],
     extras_require={
         'chalice_support': [
             'chalice>=1.16.0,<1.25.1',
-            'cuenca-validations===2.0.0.dev',
             'blinker>=1.4,<1.5',
             'mongoengine>=0.20.0,<0.23.0',
             'dnspython>=2.0.0,<2.2.0',
@@ -34,7 +45,6 @@ setup(
         'fast_support': [
             'aiobotocore>=2.0.0,<2.2.0',
             'types-aiobotocore-sqs>=2.1.0.post1,<3.0.0',
-            'cuenca-validations===2.0.0.dev',
             'fastapi>=0.115.0,<0.120.0',
             'mongoengine-plus>=0.0.2,<1.0.0',
             'starlette-context>=0.3.2,<0.4.0',
