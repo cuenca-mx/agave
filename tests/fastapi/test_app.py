10edd0e0643a8ad9069971046fa785e332bb0751
gabino <gabino@cuenca.com>
2025-01-21 17:20:29 -0600

    Replace client fixture with fastapi_client in tests

--- tests/fastapi/test_app.py
+++ tests/fastapi/test_app.py
@@ -7,32 +7,32 @@ from agave.core.exc import UnauthorizedError
 from examples.fastapi.middlewares.authed import AuthedMiddleware
 
 
-def test_iam_healthy(client: TestClient) -> None:
-    resp = client.get('/')
+def test_iam_healthy(fastapi_client: TestClient) -> None:
+    resp = fastapi_client.get('/')
     assert resp.status_code == 200
     assert resp.json() == dict(greeting="I'm healthy!!!")
 
 
-def test_cuenca_error_handler(client: TestClient) -> None:
-    resp = client.get('/raise_cuenca_errors')
+def test_cuenca_error_handler(fastapi_client: TestClient) -> None:
+    resp = fastapi_client.get('/raise_cuenca_errors')
     assert resp.status_code == 401
     assert resp.json() == dict(error='you are not lucky enough!', code=101)
 
 
-def test_fast_agave_error_handler(client: TestClient) -> None:
-    resp = client.get('/raise_fast_agave_errors')
+def test_fast_agave_error_handler(fastapi_client: TestClient) -> None:
+    resp = fastapi_client.get('/raise_fast_agave_errors')
     assert resp.status_code == 401
     assert resp.json() == dict(error='nice try!')
 
 
 def test_fast_agave_error_handler_from_middleware(
-    client: TestClient, monkeypatch: MonkeyPatch
+    fastapi_client: TestClient, monkeypatch: MonkeyPatch
 ) -> None:
     monkeypatch.setattr(
         AuthedMiddleware,
         'authorize',
         AsyncMock(side_effect=UnauthorizedError('come back to the shadows!')),
     )
-    resp = client.get('/you_shall_not_pass')
+    resp = fastapi_client.get('/you_shall_not_pass')
     assert resp.status_code == 401
     assert resp.json() == dict(error='come back to the shadows!')
