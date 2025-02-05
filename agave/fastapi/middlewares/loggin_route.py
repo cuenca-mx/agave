7770371bc909cf2f2ac22c799083f29e9abcdcca
gabino <gabino@cuenca.com>
2025-02-05 11:10:15 -0600

    Remove default stream handler from logging configuration

--- agave/fastapi/middlewares/loggin_route.py
+++ agave/fastapi/middlewares/loggin_route.py
@@ -17,9 +17,6 @@ from agave.core.loggers import (
 
 logging.basicConfig(
     level=logging.INFO,
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-    ],
 )
 
 logger = logging.getLogger(__name__)
--- agave/version.py
+++ agave/version.py
@@ -1,1 +1,1 @@
-__version__ = '1.0.2.dev10'
+__version__ = '1.0.2.dev11'
